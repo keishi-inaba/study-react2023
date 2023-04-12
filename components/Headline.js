75670c3583c5a713e37732944156ba8a7693a63b
keishi.i <gajyumaru1103@gmail.com>
2023-04-12 20:24:35 +0900

    ♻️ メイン部分のコンポーネント化

--- components/Headline.js
+++ components/Headline.js
@@ -2,14 +2,14 @@ import styles from '@/styles/Home.module.css';
 
 
 export function Headline(props) {
-  // console.log(props.title);
   return (
     <div className={styles.title}>
       <h1>{props.title}</h1>
       <p>
         Get started by editing&nbsp;
-        <code className={styles.code}>pages/{props.page}.js</code>
+        {props.page}.js
       </p>
+
     </div>
   )
 }
